1cf41a358f650c9522538ac5e21cc63c4f867176
Mohiduzzaman Mahin <mahin.swe.14@gmail.com>
2024-01-19 20:22:54 +0600

    Create 1107B.cpp

---
+++ 1107B.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+  
+    ll t,a,b;cin>>t;
+    while(t--)
+    {
+        cin>>a>>b;
+        cout<<setprecision(30)<<9*(a-1)+b<<endl;
+    }
+    return 0;
+}
